5aa36d635e9b245244334144001d2607c83c32b6
arasdenizhan <denozhan@hotmail.com>
2022-06-23 16:17:19 +0300

    Adds App Icon

--- src/main/java/com/denzhn/employeesystem/ESApplication.java
+++ src/main/java/com/denzhn/employeesystem/ESApplication.java
@@ -3,6 +3,7 @@ package com.denzhn.employeesystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class ESApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ESApplication.class.getResource("es-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 621, 554);
+        stage.getIcons().add(new Image(ESApplication.class.getResourceAsStream("hr.png")));
         stage.setResizable(false);
         stage.setTitle("Employee Management System - v1.0.0 ALPHA");
         stage.setScene(scene);
